8d596bf032d33ac42e0eb4b9813fc65124291748
ИСТОРИН ВЛАД <xloksik3@gmail.com>
2020-11-05 12:05:33 +0200

    Добавьте файлы проекта.

---
+++ Mercury/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mercury
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int i = int.Parse(r1.Text);
+                string s = (r1.Text);
+                for (i = 0; i < s.Length / 2; ++i)
+                {
+                    if (s[i] != s[s.Length - 1 - i])
+                        R1.Text = "Палиндром";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
